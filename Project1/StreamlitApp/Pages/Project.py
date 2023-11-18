877701673f205f3a7a357b8f704fe4f3aab4ee51
Aurélie M <99876342+AurelieMrtn@users.noreply.github.com>
2023-11-18 12:08:32 +0100

    .

--- Project1/StreamlitApp/Pages/Project.py
+++ Project1/StreamlitApp/Pages/Project.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import json
 
 def run():
-    st.title("Homepage")
-    st.write("Welcome to this Streamlit application: Information Retrieval Challenge.")
+    # Style
+    st.title("Information Retrieval")
+
+    # Text input
+    input = st.text_input("Type a query number:", "PLAIN-1")
+    
+    file_path = 'combined_scores.json'
+
+    with open(file_path, 'r') as file:
+        loaded_dict = json.load(file)
+    print(f"The dictionary has been loaded from {file_path}: {loaded_dict}")
+    
