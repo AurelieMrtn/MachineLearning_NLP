c766f5522207f66c1aa1b3d4ab0ceb2ef63ad933
Aurélie M <99876342+AurelieMrtn@users.noreply.github.com>
2023-11-18 13:00:42 +0100

    test

--- Project1/StreamlitApp/Pages/Project.py
+++ Project1/StreamlitApp/Pages/Project.py
@@ -13,7 +13,8 @@ def run():
     
     # Import combined_scores
     current_directory = os.getcwd()
-    file_path = current_directory + '/StreamlitApp/Pages/combined_scores.json'
+    #file_path = current_directory + '/StreamlitApp/Pages/combined_scores.json'
+    file_path = current_directory + '/StreamlitApp/Pages/requirements.txt'
     
     with open(file_path, 'r') as file:
         combined_scores = json.load(file)
