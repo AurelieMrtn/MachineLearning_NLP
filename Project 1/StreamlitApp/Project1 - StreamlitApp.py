e71e0aea45236db24a10c81d6694c791ee6cc661
Aurélie M <99876342+AurelieMrtn@users.noreply.github.com>
2023-11-18 11:47:48 +0100

    .

--- Project 1/StreamlitApp/Project1 - StreamlitApp.py
+++
@@ -1,9 +0,0 @@
-# Import combined_sores
-import json
-
-file_path = 'combined_scores.json'
-
-with open(file_path, 'r') as file:
-    loaded_dict = json.load(file)
-
-print(f"The dictionary has been loaded from {file_path}: {loaded_dict}")
