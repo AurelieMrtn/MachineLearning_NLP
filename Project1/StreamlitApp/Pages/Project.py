19e7d0b0b51bceaff9a1667c174220826507a0fa
Aurélie M <99876342+AurelieMrtn@users.noreply.github.com>
2023-11-18 12:54:31 +0100

    Update Project.py

--- Project1/StreamlitApp/Pages/Project.py
+++ Project1/StreamlitApp/Pages/Project.py
@@ -14,24 +14,16 @@ def run():
     # Import combined_scores
     current_directory = os.getcwd()
     file_path = current_directory + '/StreamlitApp/Pages/combined_scores.json'
-    file_URL = 'https://drive.google.com/uc?export=download&id=1vhfW6KvAit-v0YBoTbpqSSnPPjs1X3N9'
     
-    def download_file(url):
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
-    
-    file_content = download_file(file_URL)
-    print(file_content)
-    
-
+    with open(file_path, 'r') as file:
+        combined_scores = json.load(file)
+    print(combined_scores)
     
     if query_input in combined_scores:
         # Find the document with the highest score for the given query
         highest_score_doc = max(combined_scores[query_input], key=combined_scores[query_input].get)
         highest_score = combined_scores[query_input][highest_score_doc]
 
-        # Display the result
         st.write("Output :")
         st.write(f"Document with the highest score for query '{query_input}': {highest_score_doc} (Score: {highest_score})")
     else:
