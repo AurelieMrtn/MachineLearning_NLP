8c96aefcff95aff4ebf0f66f21c0fc8b55a851b3
Aurélie M <99876342+AurelieMrtn@users.noreply.github.com>
2024-01-24 19:00:23 +0100

    .

--- Projet2/StreamlitApp/Pages/InfoRetriev.py
+++ Projet2/StreamlitApp/Pages/InfoRetriev.py
@@ -1,34 +1,34 @@
 import streamlit as st
-import csv
 import pandas as pd
 import os
 
 def run():
+    # Title
+    st.title("Review Information Retrieval")
 
-    # Style
-    st.title("Restaurant Information Retrieval")
-    
-    # Import combined_scores
+    # Load review data
     current_directory = os.getcwd()
-    file_path = current_directory + '/Projet2/StreamlitApp/Pages/restaurants.csv'
-    
-    restaurants = pd.read_csv(file_path)
+    review_file_path = current_directory + '/Projet2/StreamlitApp/Pages/translated_reviews.csv'
+    reviews = pd.read_csv(review_file_path)
 
-    restaurant_input = st.text_input("Enter a restaurant name:")
+    # User input for restaurant name
+    review_input = st.text_input("Enter a review:")
 
     # Filter the DataFrame based on the entered restaurant name
-    if restaurant_input:
-        filtered_restaurants = restaurants[restaurants['Restaurant_Name'].str.contains(restaurant_input, case=False, na=False)]
-        
-        if not filtered_restaurants.empty:
-            # Displaying the information about the restaurant
-            for _, row in filtered_restaurants.iterrows():
-                st.subheader(row['Restaurant_Name'])
-                st.write(f"Ranking: {row['Ranking']}")
-                st.write(f"Price Range: {row['Price_Range']}")
-                st.write(f"Cuisine Type: {row['Cuisine_Type']}")
-                st.write(f"Menu Items: {row['Menu Items']}")
+    if review_input:
+        filtered_reviews = reviews[reviews['Restaurant_Name'].str.contains(review_input, case=False, na=False)]
+
+        # Display reviews for the restaurant
+        if not filtered_reviews.empty:
+            st.subheader("Reviews")
+            for _, review_row in filtered_reviews.iterrows():
+                st.write(f"Name of the restaurant: {review_row['Restaurant_Name']}")
+                st.write(f"Global Rating: {review_row['Global_Ranking']}")
+                st.write(f"Meal Rating: {review_row['Plats_Ranking']}")
+                st.write(f"Service Rating: {review_row['Service_Ranking']}")
+                st.write(f"Atmoshpere Rating: {review_row['Atmosphere_Ranking']}")
+                st.write(f"Review: {review_row['Text']}")
         else:
-            st.write("No results found for the entered restaurant name.")
+            st.write("This review doesn't exist.")
     else:
-        st.write("Please enter a restaurant name to search.")
+        st.write("Please enter a review to search.")
