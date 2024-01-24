2baf67f50d8655a05083583acddce31701128908
Aurélie M <99876342+AurelieMrtn@users.noreply.github.com>
2024-01-24 19:58:05 +0100

    Update Home.py

--- Projet2/StreamlitApp/Pages/Home.py
+++ Projet2/StreamlitApp/Pages/Home.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 def run():
     st.title("Homepage")
-    st.write("Welcome to this Streamlit application: Introduction to machine learning for normal language programmation.")
+    st.write("Welcome to this Streamlit application:" 
+             "\n Project: Data Exploration and NLP Modeling.")
